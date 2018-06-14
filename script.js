682c51983f58e8d93edac52221b923a6782c3895
Vijay Kumar <vkvijaykumar14@gmail.com>
2018-06-14 17:20:37 +0530

    hover effect solved & menu button positin fixed

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 $(function () {
   $(window).scroll(function () {
       var scrollTop = $(window).scrollTop();
+      console.log(scrollTop)
       if (scrollTop > 300){
-        $('.intro').fadeOut(1000);
+        $('.intro').animate({
+          'opacity': '0',
+          'display': 'relative',
+          'margin-bottom': '-100px'
+        }, '100');
+
       }else {
-        $('.intro').fadeIn(1000);
+        $('.intro').animate({
+          'opacity': '1',
+          'margin-bottom': '0px'
+        })
       }
   })
 })
