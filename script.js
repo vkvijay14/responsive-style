1f353ff177ae2cd073ce2215cddfb1267cb4d388
Vijay Kumar <vkvijaykumar14@gmail.com>
2018-06-15 17:05:01 +0530

    jQuery animation effect done in intro

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 $(function () {
   $(window).scroll(function () {
-      var scrollTop = $(window).scrollTop();
-      console.log(scrollTop)
-      if (scrollTop > 300){
-        $('.intro').animate({
-          'opacity': '0',
-          'display': 'relative',
-          'margin-bottom': '-100px'
-        }, '100');
-
-      }else {
-        $('.intro').animate({
-          'opacity': '1',
-          'margin-bottom': '0px'
-        })
-      }
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop > 300) {
+      $('.intro').addClass('hidden');
+      console.log('.logo-button')
+      $('.logo-button').addClass('visible');
+    } else {
+      $('.intro').removeClass('hidden');
+      $('.logo-button').removeClass('visible');
+    }
   })
-})
+});
