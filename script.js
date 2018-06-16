16a8e8ab19ad8399724a71fe324d48f8c66ca630
Vijay Kumar <vkvijaykumar14@gmail.com>
2018-06-16 18:24:51 +0530

    slide navigation function updated

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(function () {
+  // INTRO FADE IN AND OUT
   $(window).scroll(function () {
     var scrollTop = $(window).scrollTop();
     if(scrollTop > 300) {
@@ -10,4 +11,17 @@ $(function () {
       $('.logo-button').removeClass('visible');
     }
   })
+  // /INTRO FADE IN AND OUT
+  // SLIDE NAV OPEN AND CLOSE
+   $('.toggle').click(function (){
+      // $('#slideNav').toggle();
+      console.log('.toggle')
+      $('#slideNav').addClass('show');
+  })
+
+   $('#closeButton').click(function (){
+      console.log('.closeButton')
+      $('#slideNav').removeClass('show');
+  })
+  // /SLIDE NAV OPEN AND CLOSE
 });
