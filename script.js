6bf0e0e6b17aada54bba528aee413814cb964907
Vijay Kumar <vkvijaykumar14@gmail.com>
2018-06-19 15:29:40 +0530

    social button updated

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 $(function () {
+
   // INTRO FADE IN AND OUT
+  var scrollCheck = $('.intro').offset().top
+  // console.log(scrollCheck)
+
   $(window).scroll(function () {
     var scrollTop = $(window).scrollTop();
-    if(scrollTop > 300) {
+    console.log(scrollTop)
+    if(scrollTop > scrollCheck) {
       $('.intro').addClass('hidden');
-      console.log('.logo-button')
       $('.logo-button').addClass('visible');
     } else {
       $('.intro').removeClass('hidden');
       $('.logo-button').removeClass('visible');
     }
   })
-  // /INTRO FADE IN AND OUT
-  // SLIDE NAV OPEN AND CLOSE
-   $('.toggle').click(function (){
-      // $('#slideNav').toggle();
-      console.log('.toggle')
-      $('#slideNav').addClass('show');
-  })
 
-   $('#closeButton').click(function (){
-      console.log('.closeButton')
-      $('#slideNav').removeClass('show');
+  // SLIDE NAV OPEN AND CLOSE
+  $('.toggle').click(function () {
+    // $('#slideNav').toggle();
+    console.log('.toggle')
+    $('#slideNav').addClass('show');
   })
-  // /SLIDE NAV OPEN AND CLOSE
+  $('#closeButton').click(function () {
+    console.log('.closeButton')
+    $('#slideNav').removeClass('show');
+  })
 });
